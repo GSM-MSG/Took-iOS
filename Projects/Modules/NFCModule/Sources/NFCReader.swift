4b661d8466e28fd5b4859943030d60a5926e0df9
baegteun <baegteun@gmail.com>
2022-09-05 19:28:48 +0900

    :sparkles: :: NFC Reader

--- Projects/Modules/NFCModule/Sources/NFCReader.swift
+++ Projects/Modules/NFCModule/Sources/NFCReader.swift
@@ -1,8 +1,54 @@
-//
-//  NFCReader.swift
-//  NFCModuleTests
-//
-//  Created by 최형우 on 2022/09/05.
-//
+import CoreNFC
 
-import Foundation
+final class NFCReader: NSObject, NFCNDEFReaderSessionDelegate {
+    var actualData: String?
+    var nfcSession: NFCNDEFReaderSession?
+
+    func scan(data actualData: String) {
+        self.actualData = actualData
+        nfcSession = NFCNDEFReaderSession(delegate: self, queue: nil, invalidateAfterFirstRead: true)
+        nfcSession?.alertMessage = "주변 Took을 실행한 기기와 가까이해주세요!"
+        nfcSession?.begin()
+    }
+
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {}
+
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {}
+
+    func readerSession(_ session: NFCNDEFReaderSession, didDetect tags: [NFCNDEFTag]) {
+        if tags.count > 1 {
+            let retryInterval: DispatchTimeInterval = .milliseconds(500)
+            session.alertMessage = "1개 보다 많은 NFC 태그가 감지되었어요, 다시 시도해주세요!"
+            DispatchQueue.global().asyncAfter(deadline: .now() + retryInterval) {
+                session.restartPolling()
+            }
+            return
+        }
+        guard let tag = tags.first else { return }
+        Task {
+            do {
+                try await session.connect(to: tag)
+                let (status, _) = try await tag.queryNDEFStatus()
+                switch status {
+                case .readWrite:
+                    try await tag.writeNDEF(
+                        .init(records: [
+                            .wellKnownTypeTextPayload(string: actualData ?? "", locale: .current)!
+                        ])
+                    )
+                    session.alertMessage = "전송에 성공했습니다!"
+                    session.invalidate()
+                default:
+                    nfcExceptionAlert(session: session, message: "NFC 태그에 연결할 수 없어요!")
+                }
+            } catch {
+                nfcExceptionAlert(session: session, message: "NFC 태그에 연결할 수 없어요!")
+            }
+        }
+    }
+
+    private func nfcExceptionAlert(session: NFCNDEFReaderSession, message: String) {
+        session.alertMessage = message
+        session.invalidate()
+    }
+}
