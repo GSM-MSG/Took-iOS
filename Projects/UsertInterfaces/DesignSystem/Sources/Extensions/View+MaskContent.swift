0d20aad944fed7846c023e295e9c7c868c55069d
baegteun <baegteun@gmail.com>
2022-09-07 20:39:24 +0900

    :rotating_light: :: SwiftLint 반영

--- Projects/UsertInterfaces/DesignSystem/Sources/Extensions/View+MaskContent.swift
+++ Projects/UsertInterfaces/DesignSystem/Sources/Extensions/View+MaskContent.swift
@@ -7,17 +7,22 @@ public extension View {
 }
 
 public extension View {
-    func `if`<T: View>(_ conditional: Bool, transform: (Self) -> T) -> some View {
+    func `if`<T: View>(
+        _ conditional: Bool,
+        transform: (Self) -> T
+    ) -> some View {
         Group {
-            if conditional { transform(self) }
-            else { self }
+            if conditional { transform(self) } else { self }
         }
     }
-    
-    func `if`<T: View>(_ condition: Bool, true trueTransform: (Self) -> T, false falseTransform: (Self) -> T) -> some View {
+
+    func `if`<T: View>(
+        _ condition: Bool,
+        true trueTransform: (Self) -> T,
+        false falseTransform: (Self) -> T
+    ) -> some View {
         Group {
-            if condition { trueTransform(self) }
-            else { falseTransform(self) }
+            if condition { trueTransform(self) } else { falseTransform(self) }
         }
     }
 }
