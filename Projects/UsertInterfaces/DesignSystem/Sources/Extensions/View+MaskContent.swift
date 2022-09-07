256b512c2994253d940691e31408481e342170e8
baegteun <baegteun@gmail.com>
2022-09-07 20:44:48 +0900

    :sparkles: :: View + if statement

--- Projects/UsertInterfaces/DesignSystem/Sources/Extensions/View+MaskContent.swift
+++ Projects/UsertInterfaces/DesignSystem/Sources/Extensions/View+MaskContent.swift
@@ -5,24 +5,3 @@ public extension View {
         using.mask(self)
     }
 }
-
-public extension View {
-    func `if`<T: View>(
-        _ conditional: Bool,
-        transform: (Self) -> T
-    ) -> some View {
-        Group {
-            if conditional { transform(self) } else { self }
-        }
-    }
-
-    func `if`<T: View>(
-        _ condition: Bool,
-        true trueTransform: (Self) -> T,
-        false falseTransform: (Self) -> T
-    ) -> some View {
-        Group {
-            if condition { trueTransform(self) } else { falseTransform(self) }
-        }
-    }
-}
