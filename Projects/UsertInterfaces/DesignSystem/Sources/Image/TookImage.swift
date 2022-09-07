4456b0b1d582133ec5351515eca7fbfba3ac5109
baegteun <baegteun@gmail.com>
2022-09-08 08:58:21 +0900

    :bento: :: Asset 갱신

---
+++ Projects/UsertInterfaces/DesignSystem/Sources/Image/TookImage.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+public struct TookImage: View {
+    public var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct TookImage_Previews: PreviewProvider {
+    static var previews: some View {
+        TookImage()
+    }
+}
