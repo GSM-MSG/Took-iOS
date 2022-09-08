de4aa3c950e1a400a66dcd3e86c4ac2a14e48057
baegteun <baegteun@gmail.com>
2022-09-08 10:24:51 +0900

    :sparkles: :: Intro 페이지 완성

--- Projects/App/Sources/Application/TookApp.swift
+++ Projects/App/Sources/Application/TookApp.swift
@@ -10,7 +10,7 @@ struct TookApp: App {
 
     var body: some Scene {
         WindowGroup {
-            Text("Asdf")
+            AppComponent().makeRootView()
         }
     }
 }
--- Projects/Features/IntroFeature/Sources/IntroView.swift
+++ Projects/Features/IntroFeature/Sources/IntroView.swift
@@ -1,10 +1,21 @@
 import SwiftUI
+import DesignSystem
 
 public struct IntroView: View {
     public init() {}
 
     public var body: some View {
-        Text("Hello, Intro!")
+        ZStack {
+            TookImage(.background)
+                .ignoresSafeArea()
+            VStack(spacing: 16) {
+                Spacer()
+                TookButton(text: "회원가입", style: .default)
+                TookButton(text: "로그인", style: .outline)
+                    .padding(.bottom, 44)
+            }
+            .padding(.horizontal, 16)
+        }
     }
 }
 
