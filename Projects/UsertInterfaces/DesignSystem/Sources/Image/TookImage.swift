79086d396b88fa8acc44defefff8cd678faee728
baegteun <baegteun@gmail.com>
2022-09-08 09:04:33 +0900

    :sparkles: :: TookImage

--- Projects/UsertInterfaces/DesignSystem/Sources/Image/TookImage.swift
+++ Projects/UsertInterfaces/DesignSystem/Sources/Image/TookImage.swift
@@ -1,13 +1,41 @@
 import SwiftUI
 
+extension DesignSystemImages {
+    var suiImage: SwiftUI.Image {
+        SwiftUI.Image(uiImage: image)
+    }
+}
+
 public struct TookImage: View {
+    public enum Image {
+        case background
+    }
+
+    public var image: Image
+
+    public init(
+        _ image: Image
+    ) {
+        self.image = image
+    }
+
     public var body: some View {
-        Text("Hello, World!")
+        imageToImage()
+    }
+
+    private func imageToImage() -> some View {
+        switch image {
+        case .background:
+            return DesignSystemAsset.Images.background.suiImage
+                .resizable()
+                .renderingMode(.original)
+        }
     }
 }
 
 struct TookImage_Previews: PreviewProvider {
     static var previews: some View {
-        TookImage()
+        TookImage(.background)
+            .ignoresSafeArea()
     }
 }
