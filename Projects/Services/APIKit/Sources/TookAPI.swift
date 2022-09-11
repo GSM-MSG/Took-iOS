a7b4256f1d0aaa7fdece52804eda6b2cae39d6f3
baegteun <baegteun@gmail.com>
2022-09-11 20:13:15 +0900

    :sparkles: :: TookAPI

--- Projects/Services/APIKit/Sources/TookAPI.swift
+++ Projects/Services/APIKit/Sources/TookAPI.swift
@@ -4,8 +4,31 @@ import Foundation
 public protocol TookAPI: TargetType, JwtAuthorizable {
     var domain: TookDomain { get }
     var urlPath: String { get }
+    var errorMapper: [Int: TookError] { get }
+}
+
+public extension TookAPI {
+    var baseURL: URL {
+        return URL(string: "")!
+    }
+    var path: String {
+        domain.asURL + urlPath
+    }
+    var headers: [String : String]? {
+        return ["Content-Type": "application/json"]
+    }
 }
 
 public enum TookDomain: String {
     case auth
+    case businessCard = "business-cards"
+    case user
+    case image
+    case email
+}
+
+extension TookDomain {
+    var asURL: String {
+        "/\(self.rawValue)"
+    }
 }
