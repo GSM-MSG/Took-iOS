c9ad982dedcc1169c529c0b3aa5c1a07031d6da0
baegteun <baegteun@gmail.com>
2022-09-13 19:08:11 +0900

    :sparkles: :: 개인정보처리방침 URL

--- Projects/Features/SignupFeature/Sources/Terms/Terms.swift
+++ Projects/Features/SignupFeature/Sources/Terms/Terms.swift
@@ -14,7 +14,7 @@ extension Terms {
     var detailLink: URL {
         switch self {
         case .privacy:
-            return URL(string: "https://www.google.com")!
+            return URL(string: "https://shy-trust-424.notion.site/639fc1d33b0b4e938a495dda16caa4db")!
         }
     }
 }
