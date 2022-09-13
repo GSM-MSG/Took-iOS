63028a3d43dd12a0f5b92a2ce77b84c7ef47cec5
baegteun <baegteun@gmail.com>
2022-09-13 10:04:42 +0900

    :bug: :: Error Description

--- Projects/Modules/ErrorModule/Sources/TookError.swift
+++ Projects/Modules/ErrorModule/Sources/TookError.swift
@@ -13,9 +13,13 @@ extension TookError: LocalizedError {
     public var errorDescription: String? {
         switch self {
         case .unknown:
-            return "알 수 없는 에러가 일어났습니다."
+            return "알 수 없는 에러가 일어났습니다"
         case let .custom(message, _):
             return message
+        case .notVerifiedEmail:
+            return "이메일이 인증되지 않았습니다"
+        case .alreadyExistEmail:
+            return "이미 존재하는 이메일입니다"
         }
     }
 }
