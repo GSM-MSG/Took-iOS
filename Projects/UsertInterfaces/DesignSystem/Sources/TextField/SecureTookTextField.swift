fb10939099ebb46eadb95a867b702515a4223f89
baegteun <baegteun@gmail.com>
2022-09-13 11:08:32 +0900

    :sparkles: :: SecureTookTextField

---
+++ Projects/UsertInterfaces/DesignSystem/Sources/TextField/SecureTookTextField.swift
@@ -0,0 +1,88 @@
+import SwiftUI
+
+public struct SecureTookTextField: View {
+    var labelText: String
+    var placeholderText: String
+    @Binding var text: String
+    var isError: Bool
+    var errorText: String
+    var onCommit: () -> Void
+    @FocusState var isFocus: Bool
+    @State var isSecure = false
+
+    public init(
+        _ labelText: String = "",
+        placeholderText: String = "",
+        text: Binding<String>,
+        isError: Bool = false,
+        errorText: String = "",
+        onCommit: @escaping () -> Void = {}
+    ) {
+        self.labelText = labelText
+        self.placeholderText = placeholderText
+        self._text = text
+        self.isError = isError
+        self.errorText = errorText
+        self.onCommit = onCommit
+    }
+
+    public var body: some View {
+        VStack(alignment: .leading) {
+            if !labelText.isEmpty {
+                Text(labelText)
+                    .foregroundColor(Color.Took.lightGray)
+                    .tookTypo(.bold(.medium))
+            }
+            TextField("", text: $text)
+                .padding()
+                .foregroundColor(Color.Took.white)
+                .modifier(TookTextFieldSecureModifier(isSecure: $isSecure))
+                .modifier(PlaceholderStyle(placeholder: placeholderText))
+                .background(Color.Took.transparencyBoxBg)
+                .onSubmit(onCommit)
+                .cornerRadius(16)
+                .focused($isFocus)
+                .overlay {
+                    if isFocus {
+                        RoundedRectangle(cornerRadius: 16)
+                            .strokeBorder(RadialGradient.primary)
+                    } else if isError {
+                        RoundedRectangle(cornerRadius: 16)
+                            .strokeBorder(Color.Took.error)
+                    } else {
+                        RoundedRectangle(cornerRadius: 16)
+                            .strokeBorder(Color.Took.darkGray)
+                    }
+            }
+            if isError && !errorText.isEmpty {
+                Label {
+                    Text(errorText)
+                        .tookTypo(.regular(.small))
+                } icon: {
+                    Image(systemName: "exclamationmark.circle")
+                }
+                .foregroundColor(Color.Took.error)
+            }
+        }
+    }
+}
+
+struct TookTextFieldSecureModifier: ViewModifier {
+    @Binding var isSecure: Bool
+
+    func body(content: Content) -> some View {
+        ZStack {
+            content
+            HStack {
+                Spacer()
+                Button {
+                    isSecure.toggle()
+                } label: {
+                    Image(systemName: isSecure ? "eye.fill" : "eye.slash.fill")
+                        .foregroundColor(.Took.gray)
+                }
+                .padding()
+            }
+        }
+    }
+}
