ccca99aab5e4831e8dc77e2124e1de16f0c39fe1
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2022-09-22 13:39:51 +0900

    :fire: 그는 죽었어!
    그는 죽었어!

--- Projects/Services/DataMappingModule/Sources/Request/RenewalPasswordRequestDTO.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public struct RenewalPasswordRequestDTO: Equatable, Encodable {
-    public init(password: String) {
-        self.password = password
-    }
-
-    public let password: String
-}
